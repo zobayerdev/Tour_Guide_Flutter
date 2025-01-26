8d6bd231dabe66e568aa45482463d9d7dd4e4096
zobayerdev <zobayer.dev@gmail.com>
2025-01-27 00:20:04 +0600

    asset helper folder create

--- lib/networks/endpoints.dart
+++ lib/networks/endpoints.dart
@@ -22,33 +22,33 @@ final class PaymentGateway {
 final class Endpoints {
   Endpoints._();
   //backend_url
-  static String signUp() => "/api/register";
-  static String logIn() => "/api/login";
-  static String resendEmailVarificationOtp() =>
-      "/api/resend-email-verification-otp";
-  static String verifyCode() => "/api/verify-email";
-  static String logout() => "/api/logout";
-  static String forgotPasswordEmail() => "/api/forget-password";
-  static String verifyOtpFP() => "/api/verify-otp";
-  static String profile() => "/api/profile/details";
-  static String updateProfile() => "/api/profile/update";
-  static String getAllRemainders() => "/api/reminder";
-  static String addNewRemainder() => "/api/reminder";
-  static String remainder(String slug) => "/api/reminder/$slug";
-  static String deleteRemainder(int slug) => "/api/reminder/$slug";
-  static String updateRemainder(int slug) => "/api/reminder/$slug";
-  static String resetPassword() => "/api/reset-password";
-  static String changeStatus(int slug) => "/api/reminder/status/$slug";
-  static String categoryByType() => "/api/category-type";
-  static String quoteListByCategoryWithIdes(ids) =>
-      "/api/quote?per_page=1000&page=1&categories=$ids";
-  static String quoteAllList() => "/api/quote?per_page=1000&page=1";
-  static String favQuote() => "/api/favourite/quote";
-  static String addQuote() => "/api/personal-quote";
-  static String deleteFavQuote() => "/api/favourite/quote/remove";
-  static String theme() => "/api/theme";
-  static String personalQuote() => "/api/personal-quote";
-  static String deleteMyQuote(int slug) => "/api/personal-quote/$slug";
+  // static String signUp() => "/api/register";
+  // static String logIn() => "/api/login";
+  // static String resendEmailVarificationOtp() =>
+  //     "/api/resend-email-verification-otp";
+  // static String verifyCode() => "/api/verify-email";
+  // static String logout() => "/api/logout";
+  // static String forgotPasswordEmail() => "/api/forget-password";
+  // static String verifyOtpFP() => "/api/verify-otp";
+  // static String profile() => "/api/profile/details";
+  // static String updateProfile() => "/api/profile/update";
+  // static String getAllRemainders() => "/api/reminder";
+  // static String addNewRemainder() => "/api/reminder";
+  // static String remainder(String slug) => "/api/reminder/$slug";
+  // static String deleteRemainder(int slug) => "/api/reminder/$slug";
+  // static String updateRemainder(int slug) => "/api/reminder/$slug";
+  // static String resetPassword() => "/api/reset-password";
+  // static String changeStatus(int slug) => "/api/reminder/status/$slug";
+  // static String categoryByType() => "/api/category-type";
+  // static String quoteListByCategoryWithIdes(ids) =>
+  //     "/api/quote?per_page=1000&page=1&categories=$ids";
+  // static String quoteAllList() => "/api/quote?per_page=1000&page=1";
+  // static String favQuote() => "/api/favourite/quote";
+  // static String addQuote() => "/api/personal-quote";
+  // static String deleteFavQuote() => "/api/favourite/quote/remove";
+  // static String theme() => "/api/theme";
+  // static String personalQuote() => "/api/personal-quote";
+  // static String deleteMyQuote(int slug) => "/api/personal-quote/$slug";
 
 /*old
 
