ffbb6d458e5d08f47a51a5ac96103e14af9399ff
zobayerdev <zobayer.dev@gmail.com>
2025-02-05 23:52:40 +0600

    image added and icon not added, must added

--- lib/assets_helper/app_images.dart
+++ lib/assets_helper/app_images.dart
@@ -10,4 +10,10 @@ class AppImages {
   static const String iconImage = '$imageRoute/icon.png';
   static const String horizontal_line = '$imageRoute/horizontal_line.png';
   static const String google_button = '$imageRoute/google_button.png';
+  static const String adventure = '$imageRoute/adventure.png';
+  static const String culture = '$imageRoute/culture.png';
+  static const String lock = '$imageRoute/lock.png';
+  static const String nature = '$imageRoute/nature.png';
+  static const String popular = '$imageRoute/popular.png';
+  static const String verification_image = '$imageRoute/verification_image.png';
 }
